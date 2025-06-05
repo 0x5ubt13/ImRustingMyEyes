757fea76205235fa877a04752072cbd5c58f4869
0x5ubt13 <5ubt13@protonmail.com>
2025-06-05 18:49:55 +0100

    sync

--- Rust Programming KK/hello_rust/main.rs
+++ Rust Programming KK/hello_rust/main.rs
@@ -1,3 +1,51 @@
 fn main() {
     println!("Hello, world!");
+
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+    println!("The value of THREE_HOURS_IN_SECONDS is: {}", THREE_HOURS_IN_SECONDS);
+
+    let mut y = 5;
+
+    let y = 6;
+    println!("The value of y is: {}", y);
+
+    let y = y + 1;
+    println!("The value of y is: {}", y);
+
+    let y = y * 2;
+    println!("The value of y is: {}", y);
+
+    let spaces = "   ";
+    let spaces = spaces.len();
+    println!("The value of spaces is: {}", spaces);
+
+    println!("Please input something: ");
+    let mut guess = String::new();
+    std::io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    let decimal = 98_222;
+    let hex = 0xff;
+    let octal = 0o77;
+    let binary = 0b1111_0000;
+    let byte = b'A';
+    println!("decimal: {}", decimal);
+    println!("hex: {}", hex);
+    println!("octal: {}", octal);
+    println!("binary: {}", binary);
+    println!("byte: {}", byte);
+
+    let x = 2.0; // f64
+    let y: f32 = 3.0; // f32
+    println!("x: {}", x);
+    println!("y: {}", y);
+
+    let sum = 5 + 10;
+    let difference = 95.5 - 4.3;
 }
