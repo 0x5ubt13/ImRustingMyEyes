627c9cef886763db329597faa7abdc8fd1a92167
0x5utb13 <5ubt13@protonmail.com>
2025-07-12 13:31:25 +0100

    sync

--- Rust Programming KK/hello_rust/pointers_and_references.rs
+++ Rust Programming KK/hello_rust/pointers_and_references.rs
@@ -24,6 +24,15 @@ fn main() {
     let s2 = String::from("world");
     let len2 = calculate_length_borrowed(&s2); // Borrowing the string
     println!("The length of {} is {}", s2, len2); // s2 is still valid here because we borrowed it
+
+    // We can, however, create mutable references to allow modification of the borrowed value.
+    // There can only be one mutable reference to a value in a particular scope to prevent data races though.
+    let mut s3 = String::from("hello");
+    let r3 = &mut s3; // Mutable reference to s3
+    r3.push_str(", world!"); // Modify the string through the mutable reference
+    println!("Modified string: {}", r3); // Can read the modified value through the mutable reference
+
+    
 }
 
 fn calculate_length(s: String) -> usize {
